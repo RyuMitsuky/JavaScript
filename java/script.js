810731d5f9fd72951c2200fcfded22d2ee863861
Dante <juanbeltran.87.jfk@gmail.com>
2025-07-24 12:32:27 -0500

    feat :zap: add mi first commit

---
+++ java/script.js
@@ -0,0 +1,25 @@
+let boleano = true
+
+while(boleano===true) {
+
+let inicio = prompt(`hola elija una opcion
+
+1-) Hamburguesas
+2-) Categorias
+3-) chefs
+4-) ingredientes`)
+
+let opcionHamburguesa1
+
+if (opcionHamburguesa1 == 1)
+
+prompt(`holi`)
+
+}
+
+
+
+
+
+
+
